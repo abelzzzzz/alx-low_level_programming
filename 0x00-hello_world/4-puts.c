a9818df2de7ff3e0cd412866f32438a86696b3f8
abel <abelketemaaa@gmail.com>
2022-12-09 08:37:38 -0800

    asda

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 *main - Entry Point
 *return: Always 0 (success)
 */
+
 int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+      puts("\"Programming is like building a multilingual puzzle");
+                         return (0);
 }
